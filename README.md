0ab38db1669504872b9745e3e0280bfcd68176ab
Kris Kowal <kris@cixar.com>
2014-12-07 00:44:46 -0800

    FIRST!

---
+++ README.md
@@ -0,0 +1,11 @@
+
+# Dimensions
+
+Provides point and region types and methods for various dimensions.
+Supports both pure and mutable operators.
+
+
+Copyright (C) 2014 Kristopher Michael Kowal.
+All Rights Reserved.
+MIT License (enclosed).
+
---
+++ region2.js
@@ -0,0 +1,28 @@
+"use strict";
+
+var Region = require("./region");
+var Point2 = require("./point2");
+
+module.exports = Region2;
+function Region2() {
+    Region.apply(this, arguments);
+}
+
+Region2.prototype = Object.create(Region.prototype);
+Region2.prototype.constructor = Region2;
+Region2.prototype.temp1 = new Point2();
+Region2.prototype.temp2 = new Point2();
+
+Region2.prototype.contains = function (that) {
+    return (
+        this.position.x >= that.position.x &&
+        this.position.x + this.size.x <= that.position.x + that.size.x &&
+        this.position.y >= that.position.y &&
+        this.position.y + this.size.y <= that.position.y + that.size.y
+    );
+};
+
+Region2.prototype.clone = function () {
+    return new Region2(this.position.clone(), this.size.clone());
+};
+
